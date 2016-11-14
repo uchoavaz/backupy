410420127c997a8925398560db62cc23c452e398
uchoa <bioinfo@genomika.com.br>
2016-11-14 13:54:13 -0300

    status

--- postgres_bkp.py
+++ postgres_bkp.py
@@ -37,7 +37,7 @@ class Pg_Backup():
         'bkp_error': 'pg_dump -U {0} -w {1} > {2}',
         'rsync': 'echo {0} | sudo -S rsync -r {1} {2}',
         'mount': " echo {0} | sudo -S mount -t cifs '//{1}{2}' '{3}' -o username='{4}',password='{5}',rw,dir_mode=0777,file_mode=0777",
-        'umount': "echo {0} | sudo umount {1}"
+        'umount': "echo {0} | sudo -S umount {1}"
     }
 
     email = {
